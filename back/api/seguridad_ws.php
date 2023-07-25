3c4079790e8b003581ae194ca27d3d66bcd73a7b
Miguel Mise <mmise@contactcenter.local>
2023-07-25 10:59:55 -0500

    seguridad

--- back/api/seguridad_ws.php
+++ back/api/seguridad_ws.php
@@ -5,6 +5,14 @@ require_once("autoload.php");//cargador de todas las clases
 try {
     $usuario = new Usuario();//Creamos objeto tipo usuario.
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        //[1] Administrador [2] Planificador [3] Inventario [4] Reportador [5] Invitado
+        $accesos[1] = array('Inicio',"Usuarios","Proveedores",'Beneficiados','Productos','Inventario','Reglas','Planificador','Categoria','Ordenes');
+        $accesos[2] = array('Inicio',"Proveedores",'Beneficiados','Productos','Inventario','Reglas','Planificador','Categoria','Ordenes');
+        $accesos[3] = array('Inicio',"Proveedores",'Beneficiados','Productos','Inventario','Ordenes');
+        $accesos[4] = array('Inicio','Ordenes');
+        $accesos[5] = array('Inicio');
+
         $requestData = json_decode(file_get_contents('php://input'), true);
 
         $decoded_token = base64_decode($requestData['token']);
@@ -12,9 +20,9 @@ try {
         if (verifyToken($decoded_token)) {
             $resultado['mensaje'] = '¡Bienvenido!.';
             $resultado['acceso'] = 1;
-            $resultado['paginas'] = null;
             // Aumentar el tiempo de expiración en 3600 segundos
             $decodedToken = json_decode($decoded_token, true);
+            $resultado['paginas'] = $accesos[$decodedToken['rol']];
             $decodedToken['exp'] += 3600;
             // Codificar nuevamente el token modificado
             $resultado['token'] = base64_encode(json_encode($decodedToken));

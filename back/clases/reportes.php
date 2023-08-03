74705565a5b9a7f02e9ce5b329db74d17abf8ef1
Miguel Mise <92319199+miguelmise@users.noreply.github.com>
2023-08-03 10:01:14 -0500

    Update reportes.php

--- back/clases/reportes.php
+++ back/clases/reportes.php
@@ -17,6 +17,32 @@ class Reporte extends Conectar{
             return json_encode(["error" => $e->getMessage()]);
         }
     }
+
+    public function reporte_donadores($inicio,$fin){
+        try {
+            $conectar = parent::db();
+            $query = "SELECT 
+            COALESCE(NULLIF(o.orden_proveedor_nombre, ''), 'SIN NOMBRE') AS orden_proveedor_nombre,
+            o.orden_producto_codigo,
+            o.orden_producto_descripcion,
+            SUM((COALESCE(p.producto_peso_estandar, 0) * COALESCE(o.orden_producto_cantidad, 0))) AS peso,
+            SUM((COALESCE(p.producto_precio, 0) * COALESCE(o.orden_producto_cantidad, 0))) AS precio
+            FROM orden o
+            LEFT JOIN producto p ON p.producto_codigo = o.orden_producto_codigo
+            WHERE o.orden_estado = 1 AND o.orden_fecha_emision BETWEEN '{$inicio} 00:00:01' AND '{$fin} 23:59:59'
+            GROUP BY COALESCE(NULLIF(o.orden_proveedor_nombre, ''), 'SIN NOMBRE'), o.orden_producto_codigo, o.orden_producto_descripcion";
+
+            $query = $conectar->prepare($query);
+            $query->execute();
+            $result = $query->fetchAll(PDO::FETCH_ASSOC);
+            if ($result == null || !$result) {
+                $result = array();
+            }
+            return json_encode($result);
+        } catch(Exception $e){
+            return json_encode(["error" => $e->getMessage()]);
+        }
+    }
 }
 
 ?>
